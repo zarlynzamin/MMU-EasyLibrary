8c3b7cf1c60903331a08fdf1b0e9a4288ecfbac8
Zarlyn <zarlynzamin@gmail.com>
2026-06-09 19:40:30 +0800

    fixed color scheme

--- codes_mini.py
+++ codes_mini.py
@@ -4,16 +4,39 @@ import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "mini_library_secret"
+
 def create_database():
     conn = sqlite3.connect("library.db")
     cursor = conn.cursor()
 
+    # USERS TABLE
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        email TEXT,
+        password TEXT
+    )
+    """)
+
+    # ADMINS TABLE
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS admins(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT,
         email TEXT,
         password TEXT
+    )
+    """)
+
+    # BORROWED BOOKS TABLE
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS borrowed_books(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        book_title TEXT,
+        borrow_date TEXT,
+        due_date TEXT
     )
     """)
 
@@ -41,6 +64,10 @@ def load_books():
 def homepage():
     return render_template("index.html")
 
+#PROFILE#
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
 
 
 #RULES#
@@ -109,6 +136,29 @@ def borrow_book(title):
         })
 
     session["borrowed_books"] = borrowed_books
+
+    return redirect(url_for("my_books"))
+
+
+#BOOK RETURNING#
+@app.route("/return/<title>", methods=["POST"])
+def return_book(title):
+
+    borrowed_books = session.get("borrowed_books", [])
+
+    updated_books = []
+
+    for book in borrowed_books:
+
+        if isinstance(book, dict):
+            if book["title"] != title:
+                updated_books.append(book)
+
+        elif isinstance(book, str):
+            if book != title:
+                updated_books.append(book)
+
+    session["borrowed_books"] = updated_books
 
     return redirect(url_for("my_books"))
 
@@ -198,22 +248,38 @@ def admin_login():
 def admin_page():
     if "admin_logged_in" not in session:
         return redirect(url_for("admin_login"))
-    
+
+    conn = sqlite3.connect("library.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM users")
+    total_users = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM borrowed_books")
+    borrowed_count = cursor.fetchone()[0]
+
+    cursor.execute("""
+    SELECT users.username, users.email, borrowed_books.book_title
+    FROM borrowed_books
+    JOIN users ON borrowed_books.user_id = users.id
+    """)
+
+    records = cursor.fetchall()
+
+    conn.close()
+
     books = load_books()
-
-
-    total_users = 0
     total_books = len(books)
-    borrowed_books = session.get("borrowed_books", [])
-    records = []
 
-    return render_template("admin.html",
-    books=books,
-    total_users=total_users,
-    total_books=total_books,
-    borrowed_count=len(borrowed_books),
-    records=records
-)
+    return render_template(
+        "admin.html",
+        books=books,
+        total_users=total_users,
+        total_books=total_books,
+        borrowed_count=borrowed_count,
+        records=records
+    )
+
 
 #ADMIN LOGOUT#
 @app.route("/admin/logout")
@@ -235,9 +301,66 @@ def add_book():
         with open("book.txt", "a") as file:
             file.write(f"\n{title}|{category}|{summary}")
 
-        return redirect(url_for("category_page", type=category))
+        return render_template(
+            "add_book.html",
+            success=True
+        )
 
     return render_template("add_book.html")
+
+
+#REGISTER#
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+
+        print("REGISTER BUTTON CLICKED")
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("library.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        INSERT INTO users(username, email, password)
+        VALUES (?, ?, ?)
+        """, (username, email, password))
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/")
+
+    return render_template("register.html")
+
+#SIGN IN#
+@app.route("/sign_in", methods=["GET", "POST"])
+def sign_in():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("library.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        SELECT * FROM users
+        WHERE email=? AND password=?
+        """, (email, password))
+
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            session["user_id"] = user[0]
+            return redirect(url_for("homepage"))
+
+        return "Invalid email or password", 401
+
+    return render_template("sign_in.html")
+
+print(app.url_map)
 
 if __name__ == "__main__":
     app.run(debug=True)
